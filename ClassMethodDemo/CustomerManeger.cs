314f68530a9bb0e126df0ec74072d22268a1fcb6
Kaan <Kaan@DESKTOP-0T89QPA>
2022-09-21 20:23:16 +0300

    Proje dosyası ekle.

---
+++ ClassMethodDemo/CustomerManeger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    public class CustomerManeger
+    {
+        
+        
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName + " " + "Kaydınız Yapılmıştır!");
+            
+        }
+
+        public void DeleteCustomer(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName + " " + "Kaydınız Silinmiştir!");
+            
+        }
+    
+        
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using ClassMethodDemo;
+
+Customer customer1 = new Customer();
+customer1.Id = "2103021601";
+customer1.FirstName = "Kaan";
+customer1.LastName = "Yıldırım";
+
+Customer customer2 = new Customer();
+customer2.Id = "2103021602";
+customer2.FirstName = "Engin";
+customer2.LastName = "Demiroğ";
+
+Customer[] customers = new Customer[] { customer1, customer2 };
+
+CustomerManeger maneger1 = new CustomerManeger();
+maneger1.AddCustomer(customer1);
+maneger1.AddCustomer(customer2);
+Console.WriteLine("----------------------------");
+maneger1.DeleteCustomer(customer1);
+maneger1.DeleteCustomer(customer2);
+Console.WriteLine("----------------------------");
+foreach (Customer customer in customers)
+{
+    Console.WriteLine(customer.Id + " " + customer.FirstName + " " + customer.LastName);
+}
+
+
+
+
+
+
+
